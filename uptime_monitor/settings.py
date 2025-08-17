510a79544b711bbcb070d6694e37699b11b61f6f
UptimeMonitor Dev <jvwcompany115@gmail.com>
2025-08-17 21:42:38 +0200

    Fix static files warning and port binding

--- uptime_monitor/settings.py
+++ uptime_monitor/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = []
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
