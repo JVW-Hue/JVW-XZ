eeea4675680184b0bff3537a6c27e6a50f4cd67c
UptimeMonitor Dev <jvwcompany115@gmail.com>
2025-08-18 22:44:59 +0200

    FIX DATABASE - Add check_interval field to Website model

--- monitoring/models.py
+++ monitoring/models.py
@@ -5,6 +5,7 @@ class Website(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='websites')
     name = models.CharField(max_length=200)
     url = models.URLField()
+    check_interval = models.IntegerField(default=300)  # seconds
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
