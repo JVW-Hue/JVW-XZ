310f651ebf15a3932d3b8ae4f2e3a5c09b8e8a37
UptimeMonitor Dev <jvwcompany115@gmail.com>
2025-08-19 22:13:09 +0200

    FIX 404 ERRORS - Add favicon, 404 page, static files handling

--- uptime_monitor/urls.py
+++ uptime_monitor/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render, redirect
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.views.generic import RedirectView
 
 def home_view(request):
     if request.user.is_authenticated:
         return redirect('/dashboard/')
     return render(request, 'landing.html')
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +20,8 @@ urlpatterns = [
     path('auth/', include('core.urls')),
     path('dashboard/', include('monitoring.urls')),
     path('billing/', include('billing.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
